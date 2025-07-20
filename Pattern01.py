ff780b65114a27de1111da897621d1589e90a3a6
Abpattar <adityabpattar@gmail.com>
2025-07-20 21:31:17 +0530

    Simple pattern printing in Python

--- Pattern01.py
+++ Pattern01.py
@@ -2,14 +2,11 @@
 # 3 4 9 10
 # 5 6 11 12 (End goal)
 
-# Method 1: Using a pattern-based approach
-for row in [0, 1, 2]:  # 3 rows
-    for col in [0, 1, 2, 3]:  # 4 columns
+for row in [0, 1, 2]: 
+    for col in [0, 1, 2, 3]:  
         if col < 2:
-            # First two columns: consecutive numbers
             num = row * 2 + col + 1
-        else:
-            # Last two columns: numbers starting from 7
+        else: 
             num = (row * 2 + col - 2) + 7
         print(num, end=' ')
-    print()  # New line after each row
+    print()  
